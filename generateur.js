6b656ce4c4211670a93a6467cba12e726c3a4898
fherrou <fherrou@users.noreply.github.com>
2015-01-20 20:57:36 +0100

    ajout afficherSynopsis

--- generateur.js
+++ generateur.js
@@ -1,3 +1,17 @@
+
+<!--
+Copyright 2015 Florence Herrou
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
 var sujet = ['Un zadiste ', 'Un chasseur ', 'Un promoteur immobilier magouilleur ', 'Un industriel véreux ', 'Un riche héritier texan ', 'Le groupe de PJ '];
 var verbe = ['pollue ', 'occupe ', 'massacre ', 'exploite ', 'dessine ', 'se transforme en '];
 var objet = ['une ZAD', 'le président du Conseil Général', 'des éoliennes', 'un puits de pétrole', ' une mulette perlière', ' les PJ'];
@@ -6,6 +20,11 @@ var complement = [', et il aime ça !', ' sur une ZAD.', ' en rechargeant son fu
 function tirerSynopsis(){
         return sujet[Math.floor(Math.random() * sujet.length)] 
                  + verbe[Math.floor(Math.random() * verbe.length)]
-                 + objet[Math.floor(Math.random() * objet.length)]
+                + objet[Math.floor(Math.random() * objet.length)]
                  + complement[Math.floor(Math.random() * complement.length)];
+}
+
+function afficherSynopsis(var element){
+  var scenar = tirerSynopsis();
+  element.innerHTML = scenar;
 }
