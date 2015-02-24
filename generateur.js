f092323e43f4861b19fdbdef65e49b409f6e1cd1
fherrou <fherrou@users.noreply.github.com>
2015-02-24 12:03:44 +0100

    commentaires js au lieu de html

--- generateur.js
+++ generateur.js
@@ -1,5 +1,5 @@
 
-<!--
+/*
 Copyright 2015 Florence Herrou
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+*/
 
 var sujet = ['Un zadiste ', 'Un chasseur ', 'Un promoteur immobilier magouilleur ', 'Un industriel véreux ', 'Un riche héritier texan ', 'Le groupe de PJ ', 'Le peuple elfe ', 'Un naturaliste ', 'Un militant anter-mondialiste '];
 var verbe = ['pollue ', 'occupe ', 'massacre ', 'exploite ', 'dessine ', 'se transforme en ', 'signe une pétition contre ', 'mange végétarien et emmerde ', 'réchauffe ', 'voyage vers '];
